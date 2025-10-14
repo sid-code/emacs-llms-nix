f1ef20ac144cedb118515ec664e06e02f7bafd8a
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2025-10-13 21:47:58 -0700

    fix module

--- agent-shell/module.nix
+++ agent-shell/module.nix
@@ -6,11 +6,11 @@
   ...
 }:
 let
-  cfg = config.emacs.agent-shell;
+  cfg = config.programs.emacs.agent-shell;
   inherit (lib) mkIf mkEnableOption;
 in
 {
-  options.emacs.agent-shell = {
+  options.programs.emacs.agent-shell = {
     enable = mkEnableOption "emacs agent-shell package";
     # TODO: other providers
     providers.gemini = {
@@ -20,12 +20,12 @@ in
   config = {
     programs.emacs = mkIf cfg.enable {
       extraPackages = epkgs: [ epkgs.agent-shell ];
-      home.packages = mkIf cfg.providers.gemini.enable [ pkgs.gemini-cli ];
-      extraConfig = mkIf cfg.providers.gemini.emable ''
+      extraConfig = mkIf cfg.providers.gemini.enable ''
         ;; TODO: other ways to authenticate
         (setq agent-shell-google-authentication
          (agent-shell-google-make-authentication :login t))
       '';
     };
+    home.packages = mkIf cfg.providers.gemini.enable [ pkgs.gemini-cli ];
   };
 }
