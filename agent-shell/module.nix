a1ca74f7a267cd2d4bccc89b9ba5ffe006e87935
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2025-10-21 20:02:27 -0700

    add claude-code support

--- agent-shell/module.nix
+++ agent-shell/module.nix
@@ -7,27 +7,76 @@
 }:
 let
   cfg = config.programs.emacs.agent-shell;
-  inherit (lib) mkIf mkEnableOption;
+  inherit (lib)
+    mkIf
+    mkEnableOption
+    mkOption
+    types
+    ;
 in
 {
   options.programs.emacs.agent-shell = {
     enable = mkEnableOption "emacs agent-shell package";
     # TODO: other providers
-    providers.gemini = {
-      enable = mkEnableOption "emacs agent shell gemini support";
-    };
+    providers =
+      let
+        provider =
+          providerName:
+          types.submodule {
+            options = {
+              enable = mkEnableOption "${providerName} provider";
+              apiKeyFile = mkOption {
+                description = ''
+                  Path to the API key for provider ${providerName}.
+                  If not provided, will attempt to use a login flow.
+                '';
+                type = types.nullOr types.path;
+              };
+            };
+          };
+      in
+      {
+        google = mkOption {
+          type = provider "google";
+        };
+        anthropic = mkOption {
+          type = provider "anthropic";
+        };
+      };
   };
   config = {
     programs.emacs = mkIf cfg.enable {
-      extraPackages = epkgs: [ epkgs.agent-shell ];
-      extraConfig = mkIf cfg.providers.gemini.enable ''
-        (use-package agent-shell
-          :config
-          ;; TODO: other ways to authenticate
-          (setq agent-shell-google-authentication
-            (agent-shell-google-make-authentication :login t)))
-      '';
+      extraPackages = epkgs: [
+        epkgs.agent-shell
+        epkgs.f
+      ];
+      extraConfig =
+        let
+          authClause =
+            apiKeyFile: if apiKeyFile == null then ":login t" else "(f-read-text \"${apiKeyFile}\")";
+          providerAuthVariable = provider: "agent-shell-${provider}-authentication";
+          providerMakeAuthFn = provider: "agent-shell-${provider}-make-authentication";
+          providerVariableSet =
+            provider:
+            if cfg.providers.${provider}.enable then
+              "(setq ${providerAuthVariable} (${providerMakeAuthFn} ${
+                authClause cfg.providers.${provider}.apiKeyFile
+              }))"
+            else
+              "";
+        in
+        ''
+          (use-package agent-shell
+            :config
+            (require 'f)
+            ${providerVariableSet "google"}
+            ${providerVariableSet "anthropic"}
+            ${providerVariableSet "openai"})
+        '';
+
     };
-    home.packages = mkIf cfg.providers.gemini.enable [ pkgs.gemini-cli ];
+    home.packages =
+      (if cfg.providers.google.enable then [ pkgs.gemini-cli ] else [ ])
+      ++ (if cfg.providers.openai.enable then [ pkgs.codex-acp ] else [ ]);
   };
 }
