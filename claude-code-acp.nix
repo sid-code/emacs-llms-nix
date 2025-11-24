7ad0f2a94f9185defac1303d26067c8b29c1a711
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2025-11-24 13:29:20 -0800

    update claude-code-acp npm deps hash

--- claude-code-acp.nix
+++ claude-code-acp.nix
@@ -6,5 +6,5 @@ buildNpmPackage {
   pname = "claude-code-acp";
   version = "git";
   src = source;
-  npmDepsHash = "sha256-nzP2cMYKoe4S9goIbJ+ocg8bZPY/uCTOm0bLbn4m6Mw=";
+  npmDepsHash = "sha256-uXBRAiua6dafPXTSrntLiAGqkYM7Vy2zbc1yFcUjh9Y=";
 }
